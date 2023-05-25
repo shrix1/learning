6733f57b0e8fb7af3786c5817fd7743777972b7e
Shriprasanna <shriprasanna32@gmail.com>
2023-05-25 16:21:19 +0530

    remove: modern clander

--- app/Cal/page.tsx
+++ app/Cal/page.tsx
@@ -2,7 +2,6 @@ import React from "react"
 import Calendar from "react-calendar"
 import "react-calendar/dist/Calendar.css"
 import "./cal.css"
-import Cal2 from "./Cal2"
 import Anothercal from "./AnotherCal"
 
 const Page = () => {
@@ -11,7 +10,6 @@ const Page = () => {
       {/* <Calendar onChange={() => setDate} value={date} />
       {date && date?.toDateString()} */}
 
-      {/* <Cal2 /> */}
       <Anothercal />
     </main>
   )
