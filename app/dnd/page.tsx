9cf23c501ac15e5aded5f86ccae82fa728a7cf16
Shriprasanna <shriprasanna32@gmail.com>
2023-06-14 17:37:22 +0530

    added form data

--- app/dnd/page.tsx
+++ app/dnd/page.tsx
@@ -1,93 +1,172 @@
 "use client"
 import React, { useState } from "react"
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd"
+import { RxDragHandleVertical } from "react-icons/rx"
 
 const Page = () => {
   const content = [
     {
       id: 1,
-      name: "one",
+      type: "text",
+      placeholder: "Enter your name",
     },
     {
       id: 2,
-      name: "two",
+      type: "para",
+      placeholder: "Enter description",
     },
     {
       id: 3,
-      name: "three",
+      type: "radio",
+      placeholder: "edit",
     },
     {
       id: 4,
-      name: "four",
+      type: "text",
+      placeholder: "Email",
     },
   ]
 
   const [items, setItems] = useState(content)
 
   const handleOnDragEnd = (result: any) => {
-    //user dragged the item outside the droppable area
     if (!result.destination) return
-
-    //if the item is dropped in the same place it was before
     if (
       result.source.index === result.destination.index &&
       result.source.draggableId === result.destination.draggableId
     )
       return
-
-    //duplicating the array
     const newItems = [...items]
-
-    //removing the item from the array using splice method
     const [reorderedItem] = newItems.splice(result.source.index, 1)
-
-    //adding the item to the array in the destination index
-    // (adding index , removing count, item)
     newItems.splice(result.destination.index, 0, reorderedItem)
-
-    //update the state
     setItems(newItems)
   }
 
+  const [formValues, setFormValues] = useState({})
+
+  const handleType = (type: string, id: number, pl: string) => {
+    switch (type) {
+      case "text":
+        return (
+          <input
+            type="text"
+            className="border-2 border-black rounded-lg p-2"
+            name={pl}
+            onChange={(e) =>
+              setFormValues((prevValues) => ({
+                ...prevValues,
+                [pl]: e.target.value,
+              }))
+            }
+          />
+        )
+
+      case "para":
+        return (
+          <textarea
+            className="border-2 border-black rounded-lg p-2"
+            onChange={(e) =>
+              setFormValues((prevValues) => ({
+                ...prevValues,
+                [pl]: e.target.value,
+              }))
+            }
+          />
+        )
+
+      case "radio":
+        return (
+          <>
+            <input
+              type="radio"
+              name={type}
+              id={type}
+              onChange={(e) =>
+                setFormValues((prevValues) => ({
+                  ...prevValues,
+                  [type]: e.target.checked,
+                }))
+              }
+            />
+            <label htmlFor={type} className="mt-1 ml-4">
+              something
+            </label>
+          </>
+        )
+
+      default:
+        return (
+          <input
+            type="text"
+            className="border-2 border-black rounded-lg p-2"
+            onChange={(e) =>
+              setFormValues((prevValues) => ({
+                ...prevValues,
+                [pl]: e.target.value,
+              }))
+            }
+          />
+        )
+    }
+  }
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+    console.table(formValues)
+  }
+
   return (
-    <header className="flex justify-center items-center min-h-screen">
-      <DragDropContext onDragEnd={handleOnDragEnd}>
-        <Droppable droppableId="white-container">
-          {(provided, snapshot) => (
-            <main
-              {...provided.droppableProps}
-              ref={provided.innerRef}
-              className={`bg-white/50 rounded-lg w-1/2 min-h-1/2 flex justify-center p-3 text-black flex-col 
+    <>
+      <header className="flex justify-center items-center min-h-[90vh]">
+        <DragDropContext onDragEnd={handleOnDragEnd}>
+          <Droppable droppableId="white-container">
+            {(provided, snapshot) => (
+              <main
+                {...provided.droppableProps}
+                ref={provided.innerRef}
+                className={`bg-white/50 rounded-lg w-1/2 min-h-1/2 flex justify-center p-3 text-black flex-col 
               transition-colors duration-200 
               ${snapshot.isDraggingOver ? "bg-white/70" : ""}`}
-            >
-              {items.map(({ id, name }, index) => (
-                <Draggable key={id} draggableId={id.toString()} index={index}>
-                  {(provided, snapshot) => (
-                    <div
+              >
+                <form onSubmit={handleSubmit}>
+                  {items.map(({ id, type, placeholder }, index) => (
+                    <Draggable
                       key={id}
-                      {...provided.draggableProps}
-                      {...provided.dragHandleProps}
-                      ref={provided.innerRef}
-                      className={`p-3 bg-lime-200 rounded-lg border-2 border-lime-200 mb-3 transition-colors duration-200 
+                      draggableId={id.toString()}
+                      index={index}
+                    >
+                      {(provided, snapshot) => (
+                        <div
+                          key={id}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          ref={provided.innerRef}
+                          className={`flex justify-start items-center gap-10 p-3 bg-lime-200 rounded-lg border-2 border-lime-200 mb-3 transition-colors duration-200 
                       ${
                         snapshot.isDragging
                           ? "bg-lime-500 border-dashed border-black"
                           : ""
                       }`}
-                    >
-                      {name} {" | "}
-                      id: {id}
-                    </div>
-                  )}
-                </Draggable>
-              ))}
-              {provided.placeholder}
-            </main>
-          )}
-        </Droppable>
-      </DragDropContext>
-    </header>
+                        >
+                          <RxDragHandleVertical />
+                          <div className="flex items-center">
+                            {placeholder} : {handleType(type, id, placeholder)}
+                          </div>
+                        </div>
+                      )}
+                    </Draggable>
+                  ))}
+                  {provided.placeholder}
+                  <button type="submit" className="p-3 rounded-md bg-lime-300">
+                    Submit
+                  </button>
+                </form>
+              </main>
+            )}
+          </Droppable>
+        </DragDropContext>
+      </header>
+    </>
   )
 }
 
