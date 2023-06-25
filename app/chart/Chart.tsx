bfac785bed7f2c7ad5d33eccd8f621f9efaf61c0
Shriprasanna <shriprasanna32@gmail.com>
2023-06-25 19:01:28 +0530

    done area chart and pie chart

--- app/chart/Chart.tsx
+++ app/chart/Chart.tsx
@@ -1,52 +1,67 @@
 "use client"
-import { Card, Title, AreaChart } from "@tremor/react"
+import { AreaChart } from "@tremor/react"
 
 const chartdata = [
   {
     date: "Jan 22",
-    SemiAnalysis: 2890,
-    "The Pragmatic Engineer": 2338,
+    one: 2890.5,
+    two: 2338,
   },
   {
     date: "Feb 22",
-    SemiAnalysis: 2756,
-    "The Pragmatic Engineer": 2103,
+    one: 2756,
+    two: 2103,
   },
   {
     date: "Mar 22",
-    SemiAnalysis: 3322,
-    "The Pragmatic Engineer": 2194,
+    one: 3322,
+    two: 2194,
   },
   {
     date: "Apr 22",
-    SemiAnalysis: 3470,
-    "The Pragmatic Engineer": 2108,
+    one: 3470,
+    two: 2108,
   },
   {
     date: "May 22",
-    SemiAnalysis: 3475,
-    "The Pragmatic Engineer": 1812,
+    one: 3475,
+    two: 1812,
   },
   {
     date: "Jun 22",
-    SemiAnalysis: 3129,
-    "The Pragmatic Engineer": 1726,
+    one: 3129,
+    two: 1726,
   },
 ]
 
 const dataFormatter = (number: number) => {
-  return "$ " + Intl.NumberFormat("us").format(number).toString()
+  return "" + Intl.NumberFormat("us").format(number).toString()
 }
 
 const Chart = () => (
   <>
     <AreaChart
       className="h-72 mt-4"
-      data={chartdata}
-      index="date"
-      categories={["SemiAnalysis", "The Pragmatic Engineer"]}
-      colors={["indigo", "cyan"]}
-      valueFormatter={dataFormatter}
+      data={chartdata} // data to be displayed
+      index="date" // x-axis
+      categories={["one", "two"]} // tooltip when hover
+      colors={["indigo", "cyan"]} // colors for each category
+      valueFormatter={dataFormatter} // y-axis
+      showYAxis // show y-axis
+      showXAxis // show x-axis
+      showAnimation // show animation
+      showTooltip // show tooltip
+      showGridLines // show grid lines
+      showLegend // show legend 2 categories
+      showGradient // show gradient
+      yAxisWidth={100} // y-axis value (text) width
+      minValue={0}
+      maxValue={100} // y-axis min and max value
+      stack={false} // 2 curve stack on each other
+      curveType="natural" // curse style
+      connectNulls={false} // show null data by connecting the previous and next data
+      allowDecimals // decimal on tooltip
+      noDataText="no data to show" // if no data
     />
   </>
 )
--- app/chart/page.tsx
+++ app/chart/page.tsx
@@ -1,10 +1,12 @@
 import React from "react"
 import Chart from "./Chart"
+import Pie from "./Pie"
 
 const Page = () => {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
       <Chart />
+      <Pie />
     </div>
   )
 }
